68be468293f9311cd31f31e79d158abbff29cdcb
Thomas BELDAME <tbeldame@e2r6p19.42.fr>
2017-10-12 18:28:16 +0200

    Heh

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
 	<div class="footer_text">
-		Made with 🖕 by <a href="https://profile.intra.42.fr/users/tbeldame">tbeldame</a>.
+		Made by <a href="https://profile.intra.42.fr/users/tbeldame">tbeldame</a>.
 	</div>
 	<div class="footer_year">
-		2018
+		2016
 	</div>
 </div>
